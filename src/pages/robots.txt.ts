bb1cf12b463b447b88b3c386732a0c1f726af967
zwj <1927773824@qq.com>
2024-10-26 18:43:53 +0800

    feat: 更新

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,10 +2,6 @@ import type { APIRoute } from 'astro'
 
 const robotsTxt = `
 User-agent: *
-Allow: /
-
-Disallow: /_astro/
-Disallow: /fonts/
 
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
 `.trim()
